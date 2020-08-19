f733569582828189ecfa70aed7300c3da527e97b
javjrg <44872171+javjrg@users.noreply.github.com>
2020-08-18 20:38:13 -0400

    Update Modelo.R

--- Modelos/Modelo.R
+++ Modelos/Modelo.R
@@ -27,11 +27,11 @@ temp3 <- mutate(df_out_temp$Over_65, Hospitalizados=UCI*3.5)
 df_out <- list(Under_25=temp1,Adult=temp2,Over_65=temp3)
 
 #Infectividad de expuestos
-betaE = 0.0045#0.0045#0.0563
+betaE = 0.0045
 #Infectividad de Infectados leve
-betaIm = 0.009#0.009#0.1125
+betaIm = 0.009
 #Infectividad de Infectados
-betaI = 0.06#0.06#0.75
+betaI = 0.06
 
 #numero de contactos promedio
 K_g = read_rds("Bases_de_datos/Contactos.rds")
@@ -43,12 +43,11 @@ C_G_H <- read_rds("Bases_de_datos/Age_Matrix.rds")
 p_G = c(0.5, 1, 0.5)
 
 #Gammas
-alpha = 1.0
-gammaE = 0.1701#(1-alpha)*1/6 + alpha*1/4#0.1701
-gammaIm = 0.08#(1-alpha)*1/14 + alpha*1/7#0.08
-gammaI = 0.07#(1-alpha)*1/14 + alpha*1/7#0.07
-gammaH = 0.1204 #(1-alpha)*1/10 + alpha*1/2#0.1204
-gammaHc = 0.0476 #(1-alpha)*1/15 + alpha*1/10#0.0476
+gammaE = 0.1701
+gammaIm = 0.08
+gammaI = 0.07
+gammaH = 0.1204 
+gammaHc = 0.0476 
 
 #Phis
 phiEI = 0.5
@@ -61,7 +60,7 @@ phiHcD = 0.2
 Epsilon = 0.01
 
 ###Tamaño promedio de hogar
-Sigma = 3.1 #3.2 Antofagasta,   3.1 pais
+Sigma = 3.2 #3.2 Antofagasta,   3.1 pais
 
 
 
